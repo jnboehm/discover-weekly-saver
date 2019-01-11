556106dc2f055d1e684660bd1913294549de8054
Jan Niklas Böhm <mail@jnboehm.com>
2019-01-11 18:05:38 +0100

    Find 'Discover Weekly' playlist

--- discover-weekly-saver.py
+++ discover-weekly-saver.py
@@ -11,9 +11,24 @@ client_id = config.get('Login', 'client_id')
 client_secret = config.get('Login', 'client_secret')
 redirect_uri = config.get('Login', 'redirect_uri')
 
-print(user_name, client_id, client_secret, redirect_uri)
-
 scope = 'playlist-modify-public'
-util.prompt_for_user_token(user_name, scope=scope,
-                           client_id=client_id, client_secret=client_secret,
-                           redirect_uri=redirect_uri)
+tok = util.prompt_for_user_token(user_name, scope=scope,
+                                 client_id=client_id, client_secret=client_secret,
+                                 redirect_uri=redirect_uri)
+
+sp = spotipy.Spotify(auth=tok)
+
+playlists = []
+lists = sp.user_playlists(user_name)
+playlists += lists['items']
+
+lists = sp.next(lists)
+while lists is not None:
+    playlists += lists['items']
+    lists = sp.next(lists)
+
+
+def is_discover_weekly(playlist_dict):
+    return playlist_dict['name'] == 'Discover Weekly'
+
+discover_weekly = [p for p in playlists if is_discover_weekly(p)][0]
